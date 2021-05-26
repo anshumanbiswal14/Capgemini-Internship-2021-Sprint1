3622118acb5603e58d8ff993ec9dc4b020770c5e
blesy123 <blesyhelen996@gmail.com>
2021-05-26 15:59:39 +0530

    Loan Application Controller

--- Home Loan App Sprint-1 Git/src/main/java/com/ja5g4/homeloan/controller/CustomerServiceController.java
+++ Home Loan App Sprint-1 Git/src/main/java/com/ja5g4/homeloan/controller/CustomerServiceController.java
@@ -1,8 +1,17 @@
 package com.ja5g4.homeloan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ja5g4.homeloan.service.ICustomerService;
+import com.ja5g4.homeloan.service.ILoanAgreementService;
 
 @RestController
 public class CustomerServiceController {
+	
+
+@Autowired
+ICustomerService customer_service;
+
 
 }
