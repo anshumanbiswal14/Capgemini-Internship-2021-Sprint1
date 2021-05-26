df28d5882458fa4639886566c2b71096b31d8eed
Bharath Surya <bharathsurya1@gmail.com>
2021-05-26 15:44:04 +0530

    Create LoanAgreementController.java

---
+++ Home Loan App Sprint-1 Git/src/main/java/com/ja5g4/homeloan/controller/LoanAgreementController.java
@@ -0,0 +1,15 @@
+package com.ja5g4.homeloan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ja5g4.homeloan.service.ILoanAgreementService;
+
+
+@RestController
+public class LoanAgreementController {
+
+@Autowired
+ILoanAgreementService loanAgreement_service;
+
+}
