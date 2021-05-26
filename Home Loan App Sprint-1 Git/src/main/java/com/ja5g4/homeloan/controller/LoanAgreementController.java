dd9469ebfb1665f416ca576b44d3465d702488ff
Anshuman Biswal <anshumanbiswal14@gmail.com>
2021-05-26 15:52:47 +0530

    Rest Controller

--- Home Loan App Sprint-1 Git/src/main/java/com/ja5g4/homeloan/controller/LoanAgreementController.java
+++ Home Loan App Sprint-1 Git/src/main/java/com/ja5g4/homeloan/controller/LoanAgreementController.java
@@ -1,23 +1,16 @@
 package com.ja5g4.homeloan.controller;
 
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoanAgreementController {
-
-=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ja5g4.homeloan.service.ILoanAgreementService;
 
-
 @RestController
 public class LoanAgreementController {
+
 
 @Autowired
 ILoanAgreementService loanAgreement_service;
 
->>>>>>> df28d5882458fa4639886566c2b71096b31d8eed
+
 }
