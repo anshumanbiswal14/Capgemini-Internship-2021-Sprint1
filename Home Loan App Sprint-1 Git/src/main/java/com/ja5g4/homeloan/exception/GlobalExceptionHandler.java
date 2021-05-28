b199de1c820042e31591185c123b52a360c8a4f0
Gaurav Shrivastava <60589027+beingslayer@users.noreply.github.com>
2021-05-28 09:57:40 +0530

    Global Exception Handling

---
+++ Home Loan App Sprint-1 Git/src/main/java/com/ja5g4/homeloan/exception/GlobalExceptionHandler.java
@@ -0,0 +1,75 @@
+package com.ja5g4.homeloan.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(AdminApprovalException.class)
+	public ResponseEntity<String> handleAdminApprovalException(AdminApprovalException ex) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get Admin Approval");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(ex.getMessage());
+		
+	}
+	
+	
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public ResponseEntity<String> handleCustomerException(CustomerNotFoundException ex) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get a customer");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(ex.getMessage());
+		
+	}
+	
+	
+	@ExceptionHandler(FinanceVerificationException.class)
+	public ResponseEntity<String> handleFinanceException(FinanceVerificationException ex) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get a Finance Verification Officer");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(ex.getMessage());
+		
+	}
+	@ExceptionHandler(InvalidCredentialException.class)
+	public ResponseEntity<String> handleInvalidCredentialException(InvalidCredentialException ex) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get Credentials");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(ex.getMessage());
+		
+	}
+	@ExceptionHandler(InvalidLoanApplicationException.class)
+	public ResponseEntity<String> handleInvalidLoanApplicationException(InvalidLoanApplicationException ex) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get Loan Application");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(ex.getMessage());
+		
+	}
+	@ExceptionHandler(InvalidLoanAgreementException.class)
+	public ResponseEntity<String> handleInvalidLoanAggrementException(InvalidLoanAgreementException ex) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get Loan Agreement");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).body(ex.getMessage());
+		
+	}
+
+	@ExceptionHandler(LandVerificationException.class)
+	public ResponseEntity<String> handleLandException(LandVerificationException ex) {
+		HttpHeaders header = new HttpHeaders();
+		header.add("Description", "Trying to get a Land Verification Officer");
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).headers(header).build();
+		
+	}
+	
+
+}
